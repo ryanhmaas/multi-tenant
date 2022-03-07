9fb2638f2a7a50134ef651e6d6f44d5b6265cbd8
Ryan Maas <ryan.h.maas@gmail.com>
2022-03-07 11:04:12 -0500

    reuse new template

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,20 +1,20 @@
 import { NextResponse } from "next/server";
 
 export default function middleware(req) {
-  const host = "ryanhmaas.xyz";
+  const url = req.nextUrl.clone(); // clone the request url
   const { pathname } = req.nextUrl; // get pathname of request (e.g. /blog-slug)
   const hostname = req.headers.get("host"); // get hostname of request (e.g. demo.vercel.pub)
 
   console.log('hostname pre replace', hostname);
+  const host = "ryanhmaas.xyz";
 
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-      ? hostname.replace(`.${host}`, "")
-      : hostname.replace(`.localhost:3000`, "");
-
-  console.log('AHHHH currentHost', currentHost);
-  console.log('hostname', hostname);
-  console.log('pathname', pathname);
+      ? hostname
+          .replace(`.${host}`, "") // you have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
+          .replace(`.platformize.vercel.app`, "") // you can use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
+      : // in this case, our team slug is "platformize", thus *.platformize.vercel.app works
+        hostname.replace(`.localhost:3000`, "");
 
   if (pathname.startsWith(`/_sites`)) {
     console.log("app here");
@@ -22,9 +22,12 @@ export default function middleware(req) {
     return new Response(null, { status: 404 });
   }
 
+  console.log("currentHost", currentHost);
+
   if (!pathname.includes(".") && !pathname.startsWith("/api")) {
+    console.log("app here - !pathname starts");
+
     if (currentHost == "app") {
-      console.log("app here");
       if (
         pathname === "/login" &&
         (req.cookies["next-auth.session-token"] ||
@@ -32,13 +35,17 @@ export default function middleware(req) {
       ) {
         return NextResponse.redirect("/");
       }
-      return NextResponse.rewrite(`/app${pathname}`);
-    } else if (hostname === "localhost:3000" || hostname  === host) {
-      console.log('rewriting home');
-      return NextResponse.rewrite(`/home`);
+      url.pathname = `/app${pathname}`;
+      return NextResponse.rewrite(url);
+    } else if (
+      hostname === "localhost:3000" ||
+      hostname === "platformize.vercel.app"
+    ) {
+      url.pathname = `/home`;
+      return NextResponse.rewrite(url);
     } else {
-      console.log('rewriting to sites');
-      return NextResponse.rewrite(`/_sites/${currentHost}${pathname}`);
+      url.pathname = `/_sites/${currentHost}${pathname}`;
+      return NextResponse.rewrite(url);
     }
   }
 }
